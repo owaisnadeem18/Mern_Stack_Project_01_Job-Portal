2c3067fa02e20b2a65d6ed068d9b788ef8f027c6
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-04-27 16:15:02 +0500

    did few more changes in the code

--- Backend/controllers/company.controller.js
+++ Backend/controllers/company.controller.js
@@ -1,112 +1,108 @@
-import { CompanyModel } from "../models/company.model"
-
+import { CompanyModel } from "../models/company.model";
 
 // Register Company:
 
-export const RegisterCompany = async (req , res) => {
-    
-    const { companyName } = req.body
+export const RegisterCompany = async (req, res) => {
+  try {
+    const { companyName } = req.body;
 
     if (!companyName) {
-        return res.status(400).json({
-            message: "Company Name is required.",
-            success: false
-        })
+      return res.status(400).json({
+        message: "Company Name is required.",
+        success: false,
+      });
     }
 
-    let company = await CompanyModel.findOne({name: companyName})
+    let company = await CompanyModel.findOne({ name: companyName });
 
     if (company) {
-        return res.status(400).json({
-            message: "You can't register with same company name" ,
-            success: false
-        })
+      return res.status(400).json({
+        message: "You can't register with same company name",
+        success: false,
+      });
     }
 
     company = await CompanyModel.create({
-        name: companyName,
-        userId : req.id
-    })
+      name: companyName,
+      userId: req.id,
+    });
 
     return res.status(200).json({
-        message: "Company created successfully" ,
-        success: true
-    })
-
-}
-
+      message: "Company created successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Get All the Companies:
 
-export const getCompanies = async (req , res) => {
-    try {
-        
-        const userId = req.id
-        let companies = await CompanyModel.find({userId})
-        
-        if (!companies) {
-            return res.status(404).json({
-                message: "Company not found...",
-                success: false
-            })
-        }
+export const getCompanies = async (req, res) => {
+  try {
+    const userId = req.id;
+    let companies = await CompanyModel.find({ userId });
 
-    } catch (err) {
-        console.log(`Get company error is -> ${err}`)
+    if (!companies) {
+      return res.status(404).json({
+        message: "Company not found...",
+        success: false,
+      });
     }
-} 
+  } catch (err) {
+    console.log(`Get company error is -> ${err}`);
+  }
+};
 
 // Get that specific company by the I'd:
 
-export const GetCompanyById = async (req , res) => {
-    try {
-        const companyId = req.params.id;
-        const company = CompanyModel.findById({companyId})
-        if (!company) {
-            return res.status(404).json({
-                message: "Company not found",
-                success: false                
-            })
-        }
-
-        return res.status(200).json({
-            company,
-            success: true
-        })
-
-    }
-    catch(err) {
-        console.log(err)
+export const GetCompanyById = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await CompanyModel.findById({ companyId });
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found",
+        success: false,
+      });
     }
 
-}
+    return res.status(200).json({
+      company,
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export const updateCompany = async (req , res) => {
-    
-    try {
-        const {name, description, website, location } = req.body;
-        const file = req.file;
-    
-        const updateData = {name , description, website, location} 
+// Update Company:
 
-        const company = await CompanyModel.findByIdAndUpdate(req.params.id , updateData , {new: true});
+export const updateCompany = async (req, res) => {
+  try {
+    const { name, description, website, location } = req.body;
+    const file = req.file;
 
-        if (!company) {
-            return res.status(404).json({
-                message: "Company not found" ,                
-                success: false
-            })
-        }
+    const updateData = { name, description, website, location };
 
-        return res.status(200).json({
-            message: "Company Information Updated ",
-            success: true
-        })
-    
+    const company = await CompanyModel.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true }
+    );
+
+    if (!company) {
+      return res.status(404).json({
+        message: "Company not found",
+        success: false,
+      });
     }
 
-    catch(err) {
-        console.log(err)
-    }
-
-}
+    return res.status(200).json({
+      message: "Company Information Updated ",
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
