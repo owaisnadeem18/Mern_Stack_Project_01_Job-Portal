3b244af1254bb4585a361d0fb4d2888e3d236592
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-05-17 06:53:17 +0500

    rewrite code for better understanding of user applied on application

--- Backend/controllers/application.controller.js
+++ Backend/controllers/application.controller.js
@@ -1,96 +1,60 @@
-import { applicationModel } from "../models/Application.model.js"
+import { applicationModel } from "../models/Application.model"
 import { JobModel } from "../models/job.model"
 
-
-export const applyJob = async (req , res) => {
+const applyJob = async (req , res) => {
     try{
-
-        const userId = req.id 
-        const JobId = req.params.id 
-
-        if (!JobId) {
-            res.status(400).json({
-                message: "Job Id is required.",
-                success: false
-            })
-        }
-
-        // If the user already applied on the job:
-
-        const isApplied = await applicationModel.findOne({job:JobId , applicant: userId})
+        let userId = req.userId
+        let jobId = req.params.id 
         
-        if (isApplied) {
-            return res.status(400).json({
-                message: "Already Applied on this job.",
-                success: false
-            })
-        }
-
-        // If the job exists , now we have to check this condition:
-
-        const isJobExisted = await JobModel.findById(JobId)
-
-        if (!isJobExisted) {
-            return res.status(400).json({
-                message: "Job does not exist",
-                success: false 
-            })
-        }
-
-        // Here , after all the validations , now user can apply on any of the specific job: 
-        
-        const newApplication = await applicationModel.create({
-            job: JobId,
-            applicant: userId
+    // If there is no job Id:
+    if (!jobId) {
+        return res.status(404).json({
+            message: "Job Id is not found...",
+            success: false
         })
-
-        isJobExisted.applications.push(newApplication._id)
-        await isJobExisted.save()
-
-        return res.status(201).json({
-            message: "Job Applied Successfully !",
-            success: true
-        })
-
     }
 
+    // If the use already applied:
+    let ApplicationApplied = await applicationModel.find({job: jobId , applicant: userId})
 
-    catch(err){
-    
-        console.log(err)
-    
-    }
-}
-
-export const getAppliedJobs = async (req , res) => {
-
-    try {
-
-        const userId = req.id 
-
-        const PreviousApplications = await applicationModel.find({ applicant: userId }).sort({createdAt:-1}).populate({
-            path: "job",
-            options: {sort:{createdAt:-1}}
-            
-        })
-
-        if (!PreviousApplications) {
-            return res.status(404).json({
-                message: "No Applications found",
-                success: false
-            })
-        }
-
+    if (ApplicationApplied) {
         return res.status(200).json({
-            PreviousApplications,
-            success: true
-         })
-
+            message: "User Already Applied for this job",
+            success: false
+        })
     }
 
-    catch(err) {
-        console.log(err)
+    // If job does not exist:
+
+    let job = await JobModel.findById(jobId)
+    if (!job) {
+        return res.status(404).json({
+            message: "Job does not exist",
+            success: false
+        })
     }
+
+    // After all above validation , now user can apply for the job:
+
+    const createApplication = await JobModel.create({
+        jobId,
+        userId
+    })
+
+    job.applications.push(createApplication._id)
+
+    await job.save()
+
+    return res.status(201).json({
+        message: "Job Applied Successfully! ",
+        success: true
+    })
 
 }
 
+catch(err) {
+    console.log(err)
+}
+
+
+} 
