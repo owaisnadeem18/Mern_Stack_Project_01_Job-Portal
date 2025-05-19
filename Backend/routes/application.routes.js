4490d931a584cc749f3664ca4ed39ed7a0cd6c95
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-05-20 00:09:00 +0500

    Update application.routes.js

--- Backend/routes/application.routes.js
+++ Backend/routes/application.routes.js
@@ -2,11 +2,11 @@ import express from "express"
 import isAuthenticated from "../middlewares/isAuthorized.js"
 import { applyJob , GetAppliedJobs , getApplications , updateStatus } from "../controllers/application.controller.js"
 
-const router = express.Router()
+const ApplicationRouter = express.Router()
 
 router.route("/application").get(isAuthenticated, applyJob)
 router.route("/getApplicants").get(isAuthenticated, GetAppliedJobs)
 router.route("/:id/applicants").get(isAuthenticated, getApplications)
 router.route("/status/:id/update").get(isAuthenticated, updateStatus)
 
-export default router
+export default ApplicationRouter
