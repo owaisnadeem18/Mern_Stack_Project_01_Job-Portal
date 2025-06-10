ca0f39e1dbb8fe5d4dd67cdfd4bf929f37a3942b
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-10 23:38:53 +0500

    created frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen' >
+        <h1 className='bg-amber-500' >
+          Owais Nadeem
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
