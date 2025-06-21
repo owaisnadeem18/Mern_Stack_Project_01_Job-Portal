63da034c7611795bcf1965447ddc8b9a32240418
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-21 16:09:49 +0500

    created Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { Button } from './components/ui/button'
+import Navbar from './components/ui/shared/Navbar'
 
 function App() {
 
   return (
     <>
-      <div className="">
-<Button>Job Portal</Button>
-    </div>
+      <Navbar/>
     </>
   )
 }
---
+++ Frontend/src/components/ui/shared/utils/navbarItems.js
@@ -0,0 +1,1 @@
+export const navbarItems = (...menuItems) => menuItems
