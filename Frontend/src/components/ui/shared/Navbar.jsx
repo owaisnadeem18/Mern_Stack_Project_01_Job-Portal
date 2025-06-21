e690a39ed80326bc03e8e6e3a59accd6b2c11ad0
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-21 17:32:27 +0500

    created two different UI based on user logged in or logged out

--- Frontend/src/components/ui/shared/Navbar.jsx
+++ Frontend/src/components/ui/shared/Navbar.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { navbarItems } from "./utils/navbarItems";
 import { Popover, PopoverTrigger } from "@/components/ui/popover";
-import { PopoverContent } from "@radix-ui/react-popover";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { PopoverContent } from "@/components/ui/popover";
+import { Avatar , AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { LogOut, User2 } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [user , SetUser] = useState(false)
+
   return (
     <div className="Navbar items-center justify-between flex max-w-[100%] bg-cyan-950 min-h-[8vh] py-1 px-6">
       {/* 1st side of the navbar (logo)  */}
@@ -20,7 +26,7 @@ const Navbar = () => {
         </span>
       </div>
 
-      {/* 2nd side of the navbar (menu items) */}
+      {/* 2nd side of the navbar (menu items) and (avatar) */}
       <div className="menu-items flex gap-6">
         <ul className="items-center justify-between flex text-white gap-5">
           {navbarItems("Home", "Jobs", "Browse").map((item, indx) => (
@@ -34,16 +40,61 @@ const Navbar = () => {
           ))}
         </ul>
 
+        {/* Logic For Avatar */}
+
+        {
+
+          !user ? <div className="flex gap-2 items-center" >
+              <Button className={"cursor-pointer bg-cyan-800 hover:bg-cyan-900"}>
+                Login 
+              </Button>
+              <Button  className={"cursor-pointer bg-purple-600 hover:bg-purple-800 "}>
+                Signup
+              </Button>
+          </div>
+          :
+          
         <Popover>
           <PopoverTrigger asChild>
             <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarImage className={"cursor-pointer"} src="https://github.com/shadcn.png" />
             </Avatar>
           </PopoverTrigger>
-          <PopoverContent>
-            <h1 className="border-none">Owais Nadeem</h1>
+          <PopoverContent className={"w-[250px] p-4 shadow-md rounded-xl border-none space-y-2"} >
+            <div className="flex items-center" >
+
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+            </Avatar>
+            <div className="flex flex-col px-4">
+              <h6>
+                Owais Nadeem
+              </h6>
+              <p className="text-sm text-muted-foreground" >Lorem ipsum dolor sit.</p>
+            </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-center" >
+              <div className="flex items-center" >
+                <User2/>
+                <Button variant={"link"} className={"mx-2 p-0 cursor-pointer"} >
+                  View Profile
+                </Button>
+              </div>
+              
+              <div className="flex items-center" >
+                <LogOut/>
+                <Button variant={"link"} className={"mx-2 p-0 cursor-pointer hover:text-red-500"}>
+                  Logout
+                </Button>
+              </div>
+            </div>
+
           </PopoverContent>
-        </Popover>
+        </Popover> 
+
+        }
+
       </div>
     </div>
   );
