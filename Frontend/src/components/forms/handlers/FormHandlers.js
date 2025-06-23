3a0f21e2eb37a7d5cb6048bcca013b6f130302d6
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-23 11:57:18 +0500

    called POST register api from the backend

--- Frontend/src/components/forms/handlers/FormHandlers.js
+++ Frontend/src/components/forms/handlers/FormHandlers.js
@@ -1,15 +1,38 @@
-export const HandleValueChange = (e, InputValues , SetInputValues) => {
-    SetInputValues({...InputValues , [e.target.name]: e.target.value })
-    console.log("function triggered")
-}
+import { USER_API_END_POINT } from "@/utils/constant";
+import axios from "axios";
 
-export const FileHandler = (e , InputValues , SetInputValues) => {
-    SetInputValues({...InputValues , File:e.target.files?.[0]})
-}
+export const HandleValueChange = (e, InputValues, SetInputValues) => {
+  SetInputValues({ ...InputValues, [e.target.name]: e.target.value });
+};
 
-export const HandleSubmit = async (e , InputValues ) => {
-    e.preventDefault()
-    console.log("Form Submitted Successfully")
-    console.log(InputValues)
-}
+export const FileHandler = (e, InputValues, SetInputValues) => {
+  SetInputValues({ ...InputValues, File: e.target.files?.[0] });
+};
 
+export const HandleSubmit = async (e, InputValues) => {
+  e.preventDefault();
+  const formData = new FormData();
+  formData.append("FullName", InputValues.fullName);
+  formData.append("Email", InputValues.Email);
+  formData.append("PhoneNumber", InputValues.PhoneNumber);
+  formData.append("Password", InputValues.Password);
+  formData.append("Role", InputValues.Role);
+  if (InputValues.File) {
+      formData.append("File" , InputValues.File)
+  }
+
+  try {
+    const res = await axios.post(`${USER_API_END_POINT}/register` , formData , {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+        withCredentials: true
+    });
+
+
+  } catch (error) {
+
+    
+    console.error("Registration failed:", error);
+}
+};
