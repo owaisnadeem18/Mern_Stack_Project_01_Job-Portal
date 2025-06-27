1577fcf10bfb46b9b6f4ff270295c8de58b2dcd3
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-27 23:57:59 +0500

    Update LoginHandlers.js

--- Frontend/src/components/forms/handlers/LoginHandlers.js
+++ Frontend/src/components/forms/handlers/LoginHandlers.js
@@ -3,4 +3,5 @@ export const HandleSubmitLogin = (e , LoginValues , SetLoginValues) => {
     SetLoginValues({...LoginValues , [e.target.name] : e.target.value })
     console.log("Values For Name is => ", LoginValues.email)
     console.log("Values For Pasword is =>" , LoginValues.password)
+    console.log("Values For Role is is => ", LoginValues.role)
 }
