7cf3114c07b6968ffdcb6a529b56bc944166b80e
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-28 18:55:47 +0500

    redux auth slice

---
+++ Frontend/src/features/auth/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+
+    // Create Slice function has three functionalities: 
+
+    // 1. Create the key (name) which acts as the key in redux
+
+    name : "auth" , 
+
+    // 2. It creates the initial state of redux , which will get updated when you required
+    
+    initialState: {
+        loading: false
+    },
+
+    // 3. It will create the reducer , which will be updated: 
+
+    reducers: {
+      setLoading: (state , action) => {
+        state.loading = action.payload  
+      }  
+    }
+
+})
+
+export const {setLoading} = authSlice.actions
+export default authSlice.reducer
