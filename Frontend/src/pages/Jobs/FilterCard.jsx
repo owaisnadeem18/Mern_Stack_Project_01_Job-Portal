793bbf19a8663c9a73fc756e513e2bb377807c4a
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-07-02 01:37:55 +0500

    Update FilterCard.jsx

--- Frontend/src/pages/Jobs/FilterCard.jsx
+++ Frontend/src/pages/Jobs/FilterCard.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 const FilterCard = () => {
+
+  // Now, filter jobs based on the categories of industry, location and salary package
+
+  const filteredData = [
+    {
+      jobFilter: "location" ,
+      filterArray: ["Karachi" , "Lahore" , "Islamabad" , "Faisalabad"]
+    },
+    {
+      jobFilter: "industry" ,
+      filterArray : ["Frontend Developer" , "Backend Developer" , "Devops" , "AI Engineer" , "Mobile App Developer" , "Graphic Designer" , "Wordpress" ]
+    },
+    {
+      jobFilter: "salary" ,
+      filterArray: [50000 , 70000 , 100000 , 150000 , 200000 ]
+    },
+  ]
+
   return (
-    <div className='text-white' >
-      Filter Jobs
+    <div className=''>
+      <h3 className='text-white' >
+        Filter Jobs
+      </h3>
     </div>
   )
 }
