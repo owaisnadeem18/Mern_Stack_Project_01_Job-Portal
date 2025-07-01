2b87430ae6ab003c0d678863db1f11d43a3dcc7b
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-07-02 01:15:43 +0500

    updated the job section UI

--- Frontend/src/pages/Jobs/FilterCard.jsx
+++ Frontend/src/pages/Jobs/FilterCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FilterCard = () => {
   return (
-    <div>
+    <div className='text-white' >
       Filter Jobs
     </div>
   )
--- Frontend/src/pages/Jobs/Jobs.jsx
+++ Frontend/src/pages/Jobs/Jobs.jsx
@@ -10,24 +10,30 @@ const Jobs = () => {
     <>
       <Navbar />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="flex flex-col md:flex-row gap-6">
-          {/* Filter Sidebar */}
-          <aside className="w-full md:w-1/4">
-            <FilterCard />
-          </aside>
+        <div className='bg-gradient-to-br from-gray-500 to-gray-800'
+        style={{ minHeight: "calc(100vh - 56px)" }}
+        >
+            
+            <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                <div className="flex flex-col md:flex-row gap-6">
+                {/* Filter Sidebar */}
+                <aside className="w-full md:w-[12%]">
+                    <FilterCard />
+                </aside>
 
-          {/* Jobs Grid */}
-          <section className="w-full md:w-3/4">
-            <h2 className="text-2xl font-semibold mb-4">Available Jobs</h2>
-            <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2">
-              {randomJobs.map((item, indx) => (
-                <Job key={indx}  />
-              ))}
+                {/* Jobs Grid */}
+                <section className="w-full md:w-[83%]">
+                    <h2 className="text-2xl font-semibold mb-4 text-white">Available Jobs</h2>
+                    <div className="grid  gap-6 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ">
+                    {randomJobs.map((item, indx) => (
+                        <Job key={indx} />
+                    ))}
+                    </div>
+                </section>
+                </div>
             </div>
-          </section>
+      
         </div>
-      </div>
     </>
   )
 }
