2a808fdd8581c1ae03a4f38b2a01336f0d182596
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-07-01 23:25:29 +0500

    initialized the new route of job for navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
 import Home from './pages/Home/Home'
 import { Route , Routes } from 'react-router-dom'
+import Jobs from './pages/Jobs/Jobs'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home/>} ></Route>
         <Route path='/login' element={<SignIn/>} ></Route>
         <Route path='/signup' element={<SignUp/>} ></Route>
+        <Route path='/jobs' element={<Jobs />} ></Route>
       </Routes>
     </>
   )
---
+++ Frontend/src/pages/Jobs/Jobs.jsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/ui/shared/Navbar'
+import React from 'react'
+
+const Jobs = () => {
+  return (
+    <>
+        <Navbar />
+        <h2>
+            I am job component
+        </h2>
+    </>
+  )
+}
+
+export default Jobs
