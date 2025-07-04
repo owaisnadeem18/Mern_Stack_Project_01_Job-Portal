9dd3da2334e3f9ac402547a2a4a64d3f57c06ac1
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-07-04 22:13:19 +0500

    Delete src directory

--- src/components/forms/handlers/FormHandlers.js
+++
@@ -1,7 +0,0 @@
-export const HandleValueChange = (e, InputValues, SetInputValues) => {
-  SetInputValues({ ...InputValues, [e.target.name]: e.target.value });
-};
-
-export const FileHandler = (e, InputValues, SetInputValues) => {
-  SetInputValues({ ...InputValues, file: e.target.files?.[0] });
-};
--- src/components/ui/shared/utils/navbarItems.js
+++
@@ -1,1 +0,0 @@
-export const navbarItems = (...menuItems) => menuItems
