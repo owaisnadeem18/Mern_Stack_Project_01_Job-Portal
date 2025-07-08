9a77345d34b92eab362509b812c2f515fb113769
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-07-08 23:27:54 +0500

    Update CategoryCarosuel.jsx

--- Frontend/src/pages/Home/CategoryCarosuel.jsx
+++ Frontend/src/pages/Home/CategoryCarosuel.jsx
@@ -6,13 +6,12 @@ import React from 'react'
 const CategoryCarosuel = () => {
   return (
     <div>
-
         <Carousel className="w-full max-w-4xl mx-auto py-15 flex items-center justify-center " >
 
             <CarouselContent>
                 {
-                carosuelArrayItems.map((item , indx) =>
-                    <CarouselItem key={indx} className="flex justify-center items-center basis-full md:basis-1/2 lg:basis-1/3">
+                carosuelArrayItems.map((item , index) =>
+                    <CarouselItem key={index} className="flex justify-center items-center basis-full md:basis-1/2 lg:basis-1/3">
                         <Button variant="outline" className= "rounded-full" >
                             {item}
                         </Button>
