f157ea67f3f2aac642b6bf8627ae91aa7e22807a
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-07-11 23:58:22 +0500

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -127,9 +127,9 @@ export const UpdatingProfile = async (req , res) => {
 
   try {
     
-    const {fullName , email , phoneNumber , password , skills , bio} = req.body 
+    const {fullName , email , password ,  phoneNumber , skills , bio} = req.body 
 
-    console.log(fullName, email , password , phoneNumber, skills , bio)
+    console.log(fullName, email , phoneNumber, skills , bio)
 
     // Here, we have to add the data of cloudinary: 
 
