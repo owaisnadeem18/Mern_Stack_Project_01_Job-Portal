1ea6d2656c2a2f662f13938ffbf3163227f0c6c8
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-08-03 23:58:58 +0500

    Update CategoryCarosuel.jsx

--- Frontend/src/pages/Home/CategoryCarosuel.jsx
+++ Frontend/src/pages/Home/CategoryCarosuel.jsx
@@ -9,9 +9,7 @@ const CategoryCarosuel = () => {
         <Carousel className="w-full max-w-4xl mx-auto py-15 flex items-center justify-center " >
 
             <CarouselContent>
-                { 
-
-                
+                {
                 carosuelArrayItems.map((item , indx) =>
                     <CarouselItem key={indx} className="flex justify-center items-center basis-full md:basis-1/2 lg:basis-1/3">
                         <Button variant="outline" className= "rounded-full" >
